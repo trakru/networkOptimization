cf05367c6ef8801e887675415ee8fafdeac22506
Ankush Trakru <trakru@gmail.com>
2019-11-12 14:33:40 -0500

    initial solution commit

--- 001.py
+++ 001.py
@@ -1,10 +1,16 @@
+"""Simple travelling salesman problem between cities."""
+
+from __future__ import print_function
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+
+
 def create_data_model():
-    """
-    Storing the data for the problem
-    distance matrix defined as 0. New York 1. Los Angeles 2. Chicago
-    3. Minneapolis 4. Denver 5. Dallas 6. Seattle 7. Boston 8. San Francisco
-    9. St. Louis 10. Houston 11. Phoenix 12. Salt Lake City
-    """
+    """Stores the data for the problem."""
+    """0. New York 1. Los Angeles 2. Chicago 3. Minneapolis"""
+    """4. Denver 5. Dallas 6. Seattle 7. Boston 8. San Francisc"""
+    """9. St. Louis 10. Houston 11. Phoenix 12. Salt Lake City"""
     data = {}
     data['distance_matrix'] = [
         [0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972],
@@ -24,3 +30,61 @@ def create_data_model():
     data['num_vehicles'] = 1
     data['depot'] = 0
     return data
+
+
+def print_solution(manager, routing, assignment):
+    """Prints assignment on console."""
+    print('Objective: {} miles'.format(assignment.ObjectiveValue()))
+    index = routing.Start(0)
+    plan_output = 'Route for vehicle 0:\n'
+    route_distance = 0
+    while not routing.IsEnd(index):
+        plan_output += ' {} ->'.format(manager.IndexToNode(index))
+        previous_index = index
+        index = assignment.Value(routing.NextVar(index))
+        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
+    plan_output += ' {}\n'.format(manager.IndexToNode(index))
+    print(plan_output)
+    plan_output += 'Route distance: {}miles\n'.format(route_distance)
+
+
+def main():
+    """Entry point of the program."""
+    # Instantiate the data problem.
+    data = create_data_model()
+
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                           data['num_vehicles'], data['depot'])
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    # Solve the problem.
+    assignment = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if assignment:
+        print_solution(manager, routing, assignment)
+
+
+if __name__ == '__main__':
+    main()
